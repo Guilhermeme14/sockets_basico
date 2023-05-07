e7b5448f9e8047de1c8b7e09f48e6acf35daa062
Guilherme Fernandes <guifernandes111@gmail.com>
2023-05-07 17:33:10 -0300

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,25 @@
+package socket_basico;
+
+import java.net.*;
+import java.io.*;
+
+public class Server{
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket servidor = new ServerSocket(4444);
+            System.out.println("Aguardando cliente!");
+            Socket socket = servidor.accept(); // espera
+            System.out.println("Chegou o cliente :"
+                    + socket.getInetAddress().getHostAddress());
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            String mensagem = "";
+            while ((mensagem = in.readLine()) != null) {
+                System.out.println(mensagem);
+            }
+        } catch (IOException e) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
